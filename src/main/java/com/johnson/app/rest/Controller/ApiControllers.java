80c04fd088609ff7cf762b094d3fe9046da2158c
Tiffany Wise <TiffanyJWise84@gmail.com>
2021-07-29 18:59:37 -0400

    Added methods for CRUD operations

--- src/main/java/com/johnson/app/rest/Controller/ApiControllers.java
+++ src/main/java/com/johnson/app/rest/Controller/ApiControllers.java
@@ -1,15 +1,50 @@
 package com.johnson.app.rest.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.johnson.app.rest.Models.User;
+import com.johnson.app.rest.Repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ApiControllers {
 
+    @Autowired
+    private UserRepo userRepo;
+
     // API created
     @GetMapping(value = "/")
     public String getPage() {
-        return "Welcome";
+        return "It's running!";
     }
 
+    @GetMapping(value = "/users")
+    public List<User> getUsers() {
+        return userRepo.findAll();
+    }
+
+    @PostMapping(value = "/save")
+    public String saveUser(@RequestBody User user) {
+        userRepo.save(user);
+        return "Saved ...";
+    }
+
+    @PutMapping(value = "update/{id}")
+    public String updateUser(@PathVariable long id, @RequestBody User user) {
+        User updatedUser = userRepo.findById(id).get();
+        updatedUser.setFirstName(user.getFirstName());
+        updatedUser.setLastName(user.getLastName());
+        updatedUser.setOccupation(user.getOccupation());
+        updatedUser.setAge(user.getAge());
+        userRepo.save(updatedUser);
+        return "Updated ...";
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public String deleteUser(@PathVariable long id) {
+        User deletedUser = userRepo.findById(id).get();
+        userRepo.delete(deletedUser);
+        return "Delete user with the id: " + id;
+    }
 }
